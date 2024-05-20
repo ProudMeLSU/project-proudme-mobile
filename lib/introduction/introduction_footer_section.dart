a4d6c103784defa991ebe63e26228ab0966d4c45
Sumit Dhungana <sumitdhungana14@gmail.com>
2024-05-20 14:43:48 -0500

    Updated mailto UI on main screen.

--- lib/introduction/introduction_footer_section.dart
+++ lib/introduction/introduction_footer_section.dart
@@ -20,32 +20,20 @@ class IntroductionFooterWidget extends StatelessWidget {
           Text(
               regularText,
               style: TextStyle(fontSize: 16.0, fontFamily: 'Montserrat'),
+          ),
+          Center(
+            child: ElevatedButton.icon(
+              onPressed: () async {
+                String mailUrl = 'mailto:$clickableText';
+                try {
+                  await _launchUrl(Uri.parse(mailUrl));
+                } catch (e) {
+                  //Log error
+                }
+              },
+              icon: Icon(Icons.email),
+              label: Text('$clickableText'),
             ),
-          GestureDetector(
-            onTap: () async {
-              String mailUrl = 'mailto:$clickableText';
-              try {
-                await _launchUrl(Uri.parse(mailUrl));
-              } catch (e) {
-                //Log error
-              }
-            },
-            child: Container(
-              padding: EdgeInsets.all(8.0),
-              alignment: Alignment.center, 
-              decoration: BoxDecoration(
-                color: Colors.grey[300],
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Text(
-                clickableText,
-                style: TextStyle(
-                  fontSize: 16.0,
-                  fontFamily: 'Montserrat',
-                  color: Colors.blue,
-                ),
-              ),
-            )
           ),
         ],
       ),

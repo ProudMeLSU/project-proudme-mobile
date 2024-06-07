f360941240d4d7f07a679271463691de0ab2e1bd
Sumit Dhungana <sumitdhungana14@gmail.com>
2024-06-07 13:02:39 -0500

    ScreenTimeCard form and buttons functional.

--- lib/journal/screen_time.dart
+++ lib/journal/screen_time.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart'
+    show TextInputFormatter, FilteringTextInputFormatter;
 import 'package:project_proud_me/constant.dart';
 import 'package:project_proud_me/language.dart';
 
@@ -8,7 +10,228 @@ class ScreenTimeCard extends StatefulWidget {
 }
 
 class _ScreenTimeCardState extends State<ScreenTimeCard> {
+  /*TODO: Refactor the Goal/Behavior form into a separate widget
+    for better performance, use the widget on all journal behavior
+    items.
+  */
+  
   String _selectedScreenTimeType = '';
+
+  late Map<String, TextEditingController> _goalHourControllers;
+  late Map<String, TextEditingController> _goalMinuteControllers;
+  TextEditingController _goalHourController = TextEditingController();
+  TextEditingController _goalMinuteController = TextEditingController();
+
+  late Map<String, TextEditingController> _behaviorHourControllers;
+  late Map<String, TextEditingController> _behaviorMinuteControllers;
+  TextEditingController _behaviorHourController = TextEditingController();
+  TextEditingController _behaviorMinuteController = TextEditingController();
+
+  String calculateTotalGoal() {
+    int total = 0;
+
+    _goalHourControllers.forEach((key, controller) {
+      if (controller.text.isNotEmpty) {
+        int value = int.tryParse(controller.text)! * 60;
+        total += value;
+      }
+    });
+
+    _goalMinuteControllers.forEach((key, controller) {
+      if (controller.text.isNotEmpty) {
+        int value = int.tryParse(controller.text)!;
+        total += value;
+      }
+    });
+
+    return total.toString();
+  }
+
+  String calculateTotalBehavior() {
+    int total = 0;
+
+    _behaviorHourControllers.forEach((key, controller) {
+      if (controller.text.isNotEmpty) {
+        int value = int.tryParse(controller.text)! * 60;
+        total += value;
+      }
+    });
+
+    _behaviorMinuteControllers.forEach((key, controller) {
+      if (controller.text.isNotEmpty) {
+        int value = int.tryParse(controller.text)!;
+        total += value;
+      }
+    });
+
+    return total.toString();
+  }
+
+  void incrementGoalHour() {
+    setState(() {
+      if (_selectedScreenTimeType != '') {
+        if (_goalHourControllers[_selectedScreenTimeType]!.text.isEmpty) {
+          _goalHourControllers[_selectedScreenTimeType]!.text = 1.toString();
+        } else {
+          _goalHourControllers[_selectedScreenTimeType]!.text =
+              (int.parse(_goalHourControllers[_selectedScreenTimeType]!.text) +
+                      1)
+                  .toString();
+        }
+      }
+    });
+  }
+
+  void incrementBehaviorHour() {
+    setState(() {
+      if (_selectedScreenTimeType != '') {
+        if (_behaviorHourControllers[_selectedScreenTimeType]!.text.isEmpty) {
+          _behaviorHourControllers[_selectedScreenTimeType]!.text =
+              1.toString();
+        } else {
+          _behaviorHourControllers[_selectedScreenTimeType]!.text = (int.parse(
+                      _behaviorHourControllers[_selectedScreenTimeType]!.text) +
+                  1)
+              .toString();
+        }
+      }
+    });
+  }
+
+  void incrementGoalMinute() {
+    setState(() {
+      if (_selectedScreenTimeType != '') {
+        if (_goalMinuteControllers[_selectedScreenTimeType]!.text.isEmpty) {
+          _goalMinuteControllers[_selectedScreenTimeType]!.text = 1.toString();
+        } else {
+          _goalMinuteControllers[_selectedScreenTimeType]!.text = (int.parse(
+                      _goalMinuteControllers[_selectedScreenTimeType]!.text) +
+                  1)
+              .toString();
+        }
+      }
+    });
+  }
+
+  void incrementBehaviorMinute() {
+    setState(() {
+      if (_selectedScreenTimeType != '') {
+        if (_behaviorMinuteControllers[_selectedScreenTimeType]!.text.isEmpty) {
+          _behaviorMinuteControllers[_selectedScreenTimeType]!.text =
+              1.toString();
+        } else {
+          _behaviorMinuteControllers[_selectedScreenTimeType]!.text =
+              (int.parse(_behaviorMinuteControllers[_selectedScreenTimeType]!
+                          .text) +
+                      1)
+                  .toString();
+        }
+      }
+    });
+  }
+
+  void decrementGoalHour() {
+    setState(() {
+      if (_selectedScreenTimeType != '') {
+        if (_goalHourControllers[_selectedScreenTimeType]!.text.isNotEmpty &&
+            int.parse(_goalHourControllers[_selectedScreenTimeType]!.text) >
+                0) {
+          _goalHourControllers[_selectedScreenTimeType]!.text =
+              (int.parse(_goalHourControllers[_selectedScreenTimeType]!.text) -
+                      1)
+                  .toString();
+        }
+      }
+    });
+  }
+
+  void decrementBehaviorHour() {
+    setState(() {
+      if (_selectedScreenTimeType != '') {
+        if (_behaviorHourControllers[_selectedScreenTimeType]!
+                .text
+                .isNotEmpty &&
+            int.parse(_behaviorHourControllers[_selectedScreenTimeType]!.text) >
+                0) {
+          _behaviorHourControllers[_selectedScreenTimeType]!.text = (int.parse(
+                      _behaviorHourControllers[_selectedScreenTimeType]!.text) -
+                  1)
+              .toString();
+        }
+      }
+    });
+  }
+
+  void decrementGoalMinute() {
+    setState(() {
+      if (_selectedScreenTimeType != '') {
+        if (_goalMinuteControllers[_selectedScreenTimeType]!.text.isNotEmpty &&
+            int.parse(_goalMinuteControllers[_selectedScreenTimeType]!.text) >
+                0) {
+          _goalMinuteControllers[_selectedScreenTimeType]!.text = (int.parse(
+                      _goalMinuteControllers[_selectedScreenTimeType]!.text) -
+                  1)
+              .toString();
+        }
+      }
+    });
+  }
+
+  void decrementBehaviorMinute() {
+    setState(() {
+      if (_selectedScreenTimeType != '') {
+        if (_behaviorMinuteControllers[_selectedScreenTimeType]!
+                .text
+                .isNotEmpty &&
+            int.parse(
+                    _behaviorMinuteControllers[_selectedScreenTimeType]!.text) >
+                0) {
+          _behaviorMinuteControllers[_selectedScreenTimeType]!.text =
+              (int.parse(_behaviorMinuteControllers[_selectedScreenTimeType]!
+                          .text) -
+                      1)
+                  .toString();
+        }
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initGoalHourControllers();
+    _initGoalMinuteControllers();
+    _initBehaviorHourControllers();
+    _initBehaviorMinuteControllers();
+  }
+
+  void _initGoalHourControllers() {
+    _goalHourControllers = {};
+    screenTimeType.forEach((type) {
+      _goalHourControllers[type] = TextEditingController();
+    });
+  }
+
+  void _initGoalMinuteControllers() {
+    _goalMinuteControllers = {};
+    screenTimeType.forEach((type) {
+      _goalMinuteControllers[type] = TextEditingController();
+    });
+  }
+
+  void _initBehaviorHourControllers() {
+    _behaviorHourControllers = {};
+    screenTimeType.forEach((type) {
+      _behaviorHourControllers[type] = TextEditingController();
+    });
+  }
+
+  void _initBehaviorMinuteControllers() {
+    _behaviorMinuteControllers = {};
+    screenTimeType.forEach((type) {
+      _behaviorMinuteControllers[type] = TextEditingController();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -89,6 +312,17 @@ class _ScreenTimeCardState extends State<ScreenTimeCard> {
                               onChanged: (value) {
                                 setState(() {
                                   _selectedScreenTimeType = value!;
+                                  _goalHourController = _goalHourControllers[
+                                      _selectedScreenTimeType]!;
+                                  _goalMinuteController =
+                                      _goalMinuteControllers[
+                                          _selectedScreenTimeType]!;
+                                  _behaviorHourController =
+                                      _behaviorHourControllers[
+                                          _selectedScreenTimeType]!;
+                                  _behaviorMinuteController =
+                                      _behaviorMinuteControllers[
+                                          _selectedScreenTimeType]!;
                                 });
                               },
                               items: screenTimeType
@@ -124,15 +358,22 @@ class _ScreenTimeCardState extends State<ScreenTimeCard> {
                                   child: IconButton(
                                     icon: const Icon(Icons.remove),
                                     color: Colors.white,
-                                    onPressed: () {},
+                                    onPressed: () {
+                                      decrementGoalHour();
+                                    },
                                   ),
                                 ),
                                 const SizedBox(width: 10),
                                 Expanded(
                                   child: TextFormField(
+                                    controller: _goalHourController,
                                     decoration: const InputDecoration(
                                         labelText: 'Hours'),
                                     keyboardType: TextInputType.number,
+                                    onChanged: (value) => {setState(() {})},
+                                    inputFormatters: <TextInputFormatter>[
+                                      FilteringTextInputFormatter.digitsOnly
+                                    ],
                                   ),
                                 ),
                                 const SizedBox(width: 10),
@@ -144,7 +385,9 @@ class _ScreenTimeCardState extends State<ScreenTimeCard> {
                                   child: IconButton(
                                     icon: const Icon(Icons.add),
                                     color: Colors.white,
-                                    onPressed: () {},
+                                    onPressed: () {
+                                      incrementGoalHour();
+                                    },
                                   ),
                                 ),
                               ],
@@ -160,15 +403,22 @@ class _ScreenTimeCardState extends State<ScreenTimeCard> {
                                   child: IconButton(
                                     icon: const Icon(Icons.remove),
                                     color: Colors.white,
-                                    onPressed: () {},
+                                    onPressed: () {
+                                      decrementGoalMinute();
+                                    },
                                   ),
                                 ),
                                 const SizedBox(width: 10),
                                 Expanded(
                                   child: TextFormField(
+                                    controller: _goalMinuteController,
                                     decoration: const InputDecoration(
                                         labelText: 'Minutes'),
                                     keyboardType: TextInputType.number,
+                                    onChanged: (value) => {setState(() {})},
+                                    inputFormatters: <TextInputFormatter>[
+                                      FilteringTextInputFormatter.digitsOnly
+                                    ],
                                   ),
                                 ),
                                 const SizedBox(width: 10),
@@ -180,13 +430,29 @@ class _ScreenTimeCardState extends State<ScreenTimeCard> {
                                   child: IconButton(
                                     icon: const Icon(Icons.add),
                                     color: Colors.white,
-                                    onPressed: () {},
+                                    onPressed: () {
+                                      incrementGoalMinute();
+                                    },
                                   ),
                                 ),
                               ],
                             ),
                             const SizedBox(
-                              height: 20,
+                              height: 10,
+                            ),
+                            Text(
+                              'Total Goal: ${calculateTotalGoal()} Minutes',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: fontFamily,
+                                color: Theme.of(context).primaryColor,
+                              ),
+                            ),
+                            const Divider(),
+                            const SizedBox(
+                              height: 15,
                             ),
                             Text(
                               'Track My Behaviour',
@@ -209,15 +475,22 @@ class _ScreenTimeCardState extends State<ScreenTimeCard> {
                                   child: IconButton(
                                     icon: const Icon(Icons.remove),
                                     color: Colors.white,
-                                    onPressed: () {},
+                                    onPressed: () {
+                                      decrementBehaviorHour();
+                                    },
                                   ),
                                 ),
                                 const SizedBox(width: 10),
                                 Expanded(
                                   child: TextFormField(
+                                    controller: _behaviorHourController,
                                     decoration: const InputDecoration(
                                         labelText: 'Hours'),
                                     keyboardType: TextInputType.number,
+                                    onChanged: (value) => {setState(() {})},
+                                    inputFormatters: <TextInputFormatter>[
+                                      FilteringTextInputFormatter.digitsOnly
+                                    ],
                                   ),
                                 ),
                                 const SizedBox(width: 10),
@@ -229,7 +502,9 @@ class _ScreenTimeCardState extends State<ScreenTimeCard> {
                                   child: IconButton(
                                     icon: const Icon(Icons.add),
                                     color: Colors.white,
-                                    onPressed: () {},
+                                    onPressed: () {
+                                      incrementBehaviorHour();
+                                    },
                                   ),
                                 ),
                               ],
@@ -245,15 +520,22 @@ class _ScreenTimeCardState extends State<ScreenTimeCard> {
                                   child: IconButton(
                                     icon: const Icon(Icons.remove),
                                     color: Colors.white,
-                                    onPressed: () {},
+                                    onPressed: () {
+                                      decrementBehaviorMinute();
+                                    },
                                   ),
                                 ),
                                 const SizedBox(width: 10),
                                 Expanded(
                                   child: TextFormField(
+                                    controller: _behaviorMinuteController,
                                     decoration: const InputDecoration(
                                         labelText: 'Minutes'),
                                     keyboardType: TextInputType.number,
+                                    onChanged: (value) => {setState(() {})},
+                                    inputFormatters: <TextInputFormatter>[
+                                      FilteringTextInputFormatter.digitsOnly
+                                    ],
                                   ),
                                 ),
                                 const SizedBox(width: 10),
@@ -265,13 +547,29 @@ class _ScreenTimeCardState extends State<ScreenTimeCard> {
                                   child: IconButton(
                                     icon: const Icon(Icons.add),
                                     color: Colors.white,
-                                    onPressed: () {},
+                                    onPressed: () {
+                                      incrementBehaviorMinute();
+                                    },
                                   ),
                                 ),
                               ],
                             ),
                             const SizedBox(
-                              height: 20,
+                              height: 10,
+                            ),
+                            Text(
+                              'Total Behavior: ${calculateTotalBehavior()} Minutes',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: fontFamily,
+                                color: Theme.of(context).primaryColor,
+                              ),
+                            ),
+                            const Divider(),
+                            const SizedBox(
+                              height: 15,
                             ),
                             Text(
                               'Reflect',
@@ -290,7 +588,7 @@ class _ScreenTimeCardState extends State<ScreenTimeCard> {
                                   labelText: 'Type my thoughts'),
                             ),
                             const SizedBox(
-                              height: 20,
+                              height: 15,
                             ),
                             Text(
                               'AI-Generated Feedback',
